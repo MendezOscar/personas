d327d215c37b59b4cf20a0c2d8fbbed0bacbee59
Oscar Mendez <oscarmendez@MacBook-Pro-de-Oscar.local>
2019-06-06 14:46:04 -0600

    Preparando redux

---
+++ src/actions/index.js
@@ -0,0 +1,7 @@
+import http from './http'
+
+export const getUsers = () => {
+    return ( dispatch, getState ) => {
+        console.log("Se llamo a la accion getUser");
+    }
+}
--- src/views/Home.js
+++ src/views/Home.js
@@ -1,5 +1,19 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import { getUsers } from '../actions'
 
+function mapStateToPropos(state){
+    return {
+        users: state.getUsers
+    }
+}
+
+function mapDispatchToProps (dispatch){
+    return bindActionCreators([
+        getUsers
+    ], dispatch)
+}
 class Home extends Component{
     render(){
         return (
@@ -10,4 +24,7 @@ class Home extends Component{
     }
 }
 
-export default Home;
+export default connect (
+    mapStateToPropos,
+    mapDispatchToProps
+)(Home);
